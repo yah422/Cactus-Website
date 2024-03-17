713030f4a72ffe26be34b9b26ef868e8aa2206cb
Asma <saidiasma422@gmail.com>
2024-03-17 22:20:09 +0100

    add css to the shop section with the html part

--- main.js
+++ main.js
@@ -30,13 +30,13 @@ const linkAction = () =>{
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== ADD BLUR HEADER ===============*/
-const scrollHeader = () =>{
+const blurHeader = () =>{
     const header = document.getElementById('header')
     // Add a class if the bottom offset is greater than 50 of the viewport
-    this.scrollY >= 50 ? header.classList.add('scroll-header') 
-                       : header.classList.remove('scroll-header')
+    this.scrollY >= 50 ? header.classList.add('blur-header') 
+                       : header.classList.remove('blur-header')
 }
-window.addEventListener('scroll', scrollHeader)
+window.addEventListener('scroll', blurHeader)
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
