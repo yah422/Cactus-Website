a08f4471285204310611872fd2659805f951b950
Asma <saidiasma422@gmail.com>
2024-03-17 21:44:52 +0100

    add css and js to the nav and header part

--- main.js
+++ main.js
@@ -30,7 +30,13 @@ const linkAction = () =>{
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== ADD BLUR HEADER ===============*/
-
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+    // Add a class if the bottom offset is greater than 50 of the viewport
+    this.scrollY >= 50 ? header.classList.add('scroll-header') 
+                       : header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
